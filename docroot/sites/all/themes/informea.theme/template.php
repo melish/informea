23a127771b47796f70a7f17cdbabcc77f54937dd
Cătălin Dogaru <catalin.dogaru@gmail.com>
2014-07-30 17:25:55 +0300

    Added the search_box variable for page template

--- docroot/sites/all/themes/informea.theme/template.php
+++ docroot/sites/all/themes/informea.theme/template.php
@@ -3,3 +3,18 @@
  * @file
  * template.php
  */
+
+/**
+ * Preprocesses variables for page template.
+ *
+ * @param $variables
+ *   An associative array with generated variables.
+ *
+ * @return
+ *   Nothing.
+ */
+function informea_theme_preprocess_page(&$variables) {
+  $search_form = drupal_get_form('search_form');
+
+  $variables['search_box'] = drupal_render($search_form);
+}
