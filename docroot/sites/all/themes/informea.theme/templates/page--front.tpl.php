75f221667381353c3ed7de5cf673d2f72ce18bbb
Cătălin Dogaru <catalin.dogaru@gmail.com>
2014-08-01 17:33:05 +0300

    Removed the default page structure

--- docroot/sites/all/themes/informea.theme/templates/page--front.tpl.php
+++ docroot/sites/all/themes/informea.theme/templates/page--front.tpl.php
@@ -177,49 +177,6 @@
     </nav><!-- .navbar-collapse .collapse -->
   </div><!-- .container -->
 </header><!-- #navbar -->
-<div class="container">
-  <header id="page-header" role="banner">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
-    <?php print render($page['header']); ?>
-  </header><!-- #page-header -->
-  <div class="row">
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside id="sidebar-first" class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside><!-- #sidebar-first .col-sm-3 -->
-    <?php endif; ?>
-    <section<?php print $content_column_class; ?>>
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-    </section>
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside id="sidebar-second" class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside><!-- #sidebar-second .col-sm-3 -->
-    <?php endif; ?>
-  </div><!-- .row -->
-</div><!-- .container -->
 <footer class="footer">
   <div class="container">
     <?php if (!empty($page['footer'])): ?>
