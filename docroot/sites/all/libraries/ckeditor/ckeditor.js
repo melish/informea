ed0874d9075b482b7b36011d6ded2fc24d73e16e
Cristian Romanescu <cristian@romanescu.ro>
2014-08-05 22:31:06 +0300

    Fixed ckeditor issue related to version detection

--- docroot/sites/all/libraries/ckeditor/ckeditor.js
+++ docroot/sites/all/libraries/ckeditor/ckeditor.js
@@ -1,4 +1,4 @@
-﻿// version:'4.4.3',revision:'4391' 
+﻿// version:'4.4.3',revision:'4391'
 /*
 Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.md or http://ckeditor.com/license
