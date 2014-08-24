88f88e1d660f151b6c238917042d729ec1b969a2
Cristian Romanescu <cristian@romanescu.ro>
2014-08-24 21:13:54 +0300

    Added library.php to handle pre/post install tasks

--- scripts/drupal_pre_install.php
+++ scripts/drupal_pre_install.php
@@ -4,14 +4,6 @@
  * Created: 201407171712
  */
 
-$config_file = sprintf('%s/../conf/config.json', dirname(__FILE__));
-if(!is_readable($config_file)) {
-	die("Cannot read config file! ($config_file). Please configure your project correctly");
-}
-$json = json_decode(file_get_contents($config_file), TRUE);
+require_once 'library.php';
 
-if(!empty($json['variables']) && is_array($json['variables'])) {
-	foreach($json['variables'] as $k => $v) {
-		variable_set($k, $v);
-	}
-}
+project_pre_install_set_config_variables();
