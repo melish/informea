bbb96fff8065a00c63610975d4e4a87c0e73cbe6
Cătălin Dogaru <catalin.dogaru@gmail.com>
2014-11-07 14:41:34 +0200

    Added the treaties template

---
+++ docroot/sites/all/themes/informea.theme/templates/treaties.tpl.php
@@ -0,0 +1,93 @@
+<h3><?php print t('Global treaties and conventions'); ?></h3>
+<div class="text-center">
+  <div class="brand-group">
+    <a class="brand brand-cbd" target="_blank" href="<?php print url('treaties/cbd'); ?>">
+      <div class="image"></div>
+      <?php print t('CBD'); ?>
+    </a><!-- .brand .brand-cbd -->
+    <a class="brand brand-cartagena" target="_blank" href="<?php print url('treaties/cartagena'); ?>">
+      <div class="image"></div>
+      <?php print t('Cartagena'); ?>
+    </a><!-- .brand .brand-cartagena -->
+    <a class="brand brand-nagoya" target="_blank" href="<?php print url('treaties/nagoya'); ?>">
+      <div class="image"></div>
+      <?php print t('Nagoya'); ?>
+    </a><!-- .brand .brand-nagoya -->
+  </div><!-- .brand-group -->
+  <div class="brand-group">
+    <a class="brand brand-cms" target="_blank" href="<?php print url('treaties/cms'); ?>">
+      <div class="image"></div>
+      <?php print t('CMS'); ?>
+    </a><!-- .brand .brand-cms -->
+  </div><!-- .brand-group -->
+  <div class="brand-group">
+    <a class="brand brand-cites" target="_blank" href="<?php print url('treaties/cites'); ?>">
+      <div class="image"></div>
+      <?php print t('CITES'); ?>
+    </a><!-- .brand .brand-cites -->
+  </div><!-- .brand-group -->
+  <div class="brand-group">
+    <a class="brand brand-plant-treaty" target="_blank" href="<?php print url('treaties/plant-treaty'); ?>">
+      <div class="image"></div>
+      <?php print t('Plant treaty'); ?>
+    </a><!-- .brand .brand-plant-treaty -->
+  </div><!-- .brand-group -->
+  <div class="brand-group">
+    <a class="brand brand-ramsar" target="_blank" href="<?php print url('treaties/ramsar'); ?>">
+      <div class="image"></div>
+      <?php print t('Ramsar'); ?>
+    </a><!-- .brand .brand-ramsar -->
+  </div><!-- .brand-group -->
+  <div class="brand-group">
+    <a class="brand brand-whc" target="_blank" href="<?php print url('treaties/whc'); ?>">
+      <div class="image"></div>
+      <?php print t('WHC'); ?>
+    </a><!-- .brand .brand-whc -->
+  </div><!-- .brand-group -->
+  <div class="brand-group">
+    <a class="brand brand-basel" target="_blank" href="<?php print url('treaties/basel'); ?>">
+      <div class="image"></div>
+      <?php print t('Basel'); ?>
+    </a><!-- .brand .brand-basel -->
+    <a class="brand brand-rotterdam" target="_blank" href="<?php print url('treaties/rotterdam'); ?>">
+      <div class="image"></div>
+      <?php print t('Rotterdam'); ?>
+    </a><!-- .brand .brand-rotterdam -->
+    <a class="brand brand-stockholm" target="_blank" href="<?php print url('treaties/stockholm'); ?>">
+      <div class="image"></div>
+      <?php print t('Stockholm'); ?>
+    </a><!-- .brand .brand-stockholm -->
+  </div><!-- .brand-group -->
+  <div class="brand-group">
+    <a class="brand brand-minamata" target="_blank" href="<?php print url('treaties/minamata'); ?>">
+      <div class="image"></div>
+      <?php print t('Minamata'); ?>
+    </a><!-- .brand .brand-minamata -->
+  </div><!-- .brand-group -->
+  <div class="brand-group">
+    <a class="brand brand-vienna" target="_blank" href="<?php print url('treaties/vienna'); ?>">
+      <div class="image"></div>
+      <?php print t('Vienna'); ?>
+    </a><!-- .brand .brand-vienna -->
+    <a class="brand brand-montreal" target="_blank" href="<?php print url('treaties/montreal'); ?>">
+      <div class="image"></div>
+      <?php print t('Montreal'); ?>
+    </a><!-- .brand .brand-montreal -->
+  </div><!-- .brand-group -->
+  <div class="brand-group">
+    <a class="brand brand-unccd" target="_blank" href="<?php print url('treaties/unccd'); ?>">
+      <div class="image"></div>
+      <?php print t('UNCCD'); ?>
+    </a><!-- .brand .brand-unccd -->
+  </div><!-- .brand-group -->
+  <div class="brand-group">
+    <a class="brand brand-unfccc" target="_blank" href="<?php print url('treaties/unfccc'); ?>">
+      <div class="image"></div>
+      <?php print t('UNFCCC'); ?>
+    </a><!-- .brand .brand-unfccc -->
+    <a class="brand brand-kyoto" target="_blank" href="<?php print url('treaties/kyoto'); ?>">
+      <div class="image"></div>
+      <?php print t('Kyoto'); ?>
+    </a><!-- .brand .brand-kyoto -->
+  </div><!-- .brand-group -->
+</div><!-- .text-center -->
