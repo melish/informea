615a2c94ae83391cde63db6f53b414ef89719746
Cătălin Dogaru <catalin.dogaru@gmail.com>
2014-11-11 18:03:07 +0200

    Added treaties table

--- docroot/sites/all/themes/informea.theme/templates/treaties.tpl.php
+++ docroot/sites/all/themes/informea.theme/templates/treaties.tpl.php
@@ -91,3 +91,131 @@
     </a><!-- .brand .brand-kyoto -->
   </div><!-- .brand-group -->
 </div><!-- .text-center -->
+<h3><?php print t('All treaties'); ?></h3>
+<table class="table table-bordered" id="treaties-table">
+  <thead>
+    <tr>
+      <td colspan="2">
+        <small class="text-muted">
+          Showing <strong><?php print count($treaties); ?></strong> of <strong><?php print count($treaties); ?></strong> treaties
+        </small><!-- .text-muted -->
+      </td>
+      <th class="dropdown text-center">
+        <a data-toggle="dropdown" href="#">
+          Region
+          <span class="caret"></span>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-right" role="menu">
+          <li><a href="#">All regions</a></li>
+          <?php foreach ($regions as $region): ?>
+            <li><?php print l($region->name, NULL, array('fragment' => $region->tid, 'external' => TRUE)); ?></li>
+          <?php endforeach; ?>
+        </ul>
+      </th>
+      <th class="text-center">Year</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    <?php foreach ($treaties as $treaty): ?>
+      <tr>
+      <td class="text-center">
+        <?php if (property_exists($treaty, 'logo')): ?>
+          <?php
+          print theme_image(array(
+            'path' => $treaty->logo,
+            'alt' => $treaty->title,
+            'attributes' => array()
+          ));
+          ?>
+        <?php endif; ?>
+      </td><!-- .text-center -->
+      <td>
+        <h4>
+          <a href="#"><?php print $treaty->official_name; ?></a>
+          <small><?php print $treaty->title; ?></small>
+        </h4>
+        <ul class="list-inline small">
+          <li>
+            <strong>Topics:</strong>
+            Chemicals and Waste Management, Wastes including harzadous wastes
+          </li>
+        </ul><!-- .list-inline .small -->
+      </td>
+      <td class="text-center">
+        <?php print implode(', ', $treaty->regions); ?>
+      </td>
+      <td class="text-center">
+        <?php if (property_exists($treaty, 'year')): ?>
+          <?php print $treaty->year; ?>
+        <?php else: ?>
+          <?php print t('N/A'); ?>
+        <?php endif; ?>
+      </td>
+    </tr>
+    <?php endforeach; ?>
+
+    <tr>
+      <td class="text-center">
+        <img alt="" src="http://www.informea.org/wp-content/uploads/images/treaty/carpathian_logo.jpg">
+      </td><!-- .text-center -->
+      <td>
+        <h4>
+          <a href="#">Basel Convention on the Control of Transboundary Movements of Hazardous Wastes and Their Disposal</a>
+        </h4>
+        <ul class="list-inline small">
+          <li>
+            <strong>Basel Convention</strong>
+          </li>
+          <li class="text-muted">
+            <strong>Topics:</strong>
+            Chemicals and Waste Management, Wastes including harzadous wastes
+          </li>
+        </ul><!-- .list-inline .small -->
+      </td>
+      <td class="text-center">Europe</td>
+      <td class="text-center">N/A</td>
+    </tr>
+    <tr>
+      <td class="text-center">
+        <img alt="" src="http://www.informea.org/wp-content/uploads/images/treaty/logo_unece.png">
+      </td><!-- .text-center -->
+      <td>
+        <h4>
+          <a href="#">Convention on Access to Information, Public Participation in Decision-Making and Access to Justice in Environmental Matters</a>
+        </h4>
+        <ul class="list-inline small">
+          <li>
+            <strong>Aarhus Convention</strong>
+          </li>
+          <li class="text-muted">
+            <strong>Topics:</strong>
+            Access to Information
+          </li>
+          <li>
+            <a class="collapsed" data-toggle="collapse" href="#mycollapse">Toggle protocols</a>
+          </li>
+        </ul><!-- .list-inline .small -->
+        <ul class="list-group collapse" id="mycollapse">
+          <li class="list-group-item">
+            <a href="#">Cras justo odio</a>
+          </li>
+          <li class="list-group-item">
+            <a href="#">Dapibus ac facilisis in</a>
+          </li>
+          <li class="list-group-item">
+            <a href="#">Morbi leo risus</a>
+          </li>
+          <li class="list-group-item">
+            <a href="#">Porta ac consectetur ac</a>
+          </li>
+          <li class="list-group-item">
+            <a href="#">Vestibulum at eros</a>
+          </li>
+        </ul><!-- .list-group -->
+      </td>
+      <td class="text-center">Europe, North America</td>
+      <td class="text-center">1998</td>
+    </tr>
+  </tbody>
+</table><!-- .table .table-bordered #treaties-table -->
