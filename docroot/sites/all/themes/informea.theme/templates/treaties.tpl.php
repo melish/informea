7e5b2084bf4fe923b66285aa21fa7697684ba880
Cătălin Dogaru <catalin.dogaru@gmail.com>
2014-11-13 13:18:04 +0200

    Updated the treaties template

--- docroot/sites/all/themes/informea.theme/templates/treaties.tpl.php
+++ docroot/sites/all/themes/informea.theme/templates/treaties.tpl.php
@@ -116,7 +116,6 @@
     </tr>
   </thead>
   <tbody>
-
     <?php foreach ($treaties as $treaty): ?>
       <tr>
       <td class="text-center">
@@ -132,19 +131,29 @@
       </td><!-- .text-center -->
       <td>
         <h4>
-          <a href="#"><?php print $treaty->official_name; ?></a>
+          <?php print l($treaty->official_name, 'node/' . $treaty->nid); ?>
           <small><?php print $treaty->title; ?></small>
         </h4>
-        <ul class="list-inline small">
-          <li>
-            <strong>Topics:</strong>
-            Chemicals and Waste Management, Wastes including harzadous wastes
-          </li>
-        </ul><!-- .list-inline .small -->
+        <p class="small">
+          <strong>Topics:</strong>
+          <?php print $treaty->topics; ?>
+        </p><!-- .small -->
+        <?php if (property_exists($treaty, 'protocols')): ?>
+          <p class="small">
+            <a class="collapsed" data-toggle="protocols-<?php print $treaty->nid; ?>" href="#mycollapse">Toggle protocols</a>
+          </p><!-- .small -->
+          <ul class="list-group collapse" id="protocols-<?php print $treaty->nid; ?>">
+            <?php foreach ($treaty->protocols as $protocol): ?>
+              <li class="list-group-item">
+                <?php print l($protocol->title, 'node/' . $protocol->nid); ?>
+              </li><!-- .list-group-item -->
+            <?php endforeach; ?>
+          </ul><!-- .list-group .collapse -->
+        <?php endif; ?>
       </td>
       <td class="text-center">
         <?php print implode(', ', $treaty->regions); ?>
-      </td>
+      </td><!-- .text-center -->
       <td class="text-center">
         <?php if (property_exists($treaty, 'year')): ?>
           <?php print $treaty->year; ?>
@@ -154,68 +163,5 @@
       </td>
     </tr>
     <?php endforeach; ?>
-
-    <tr>
-      <td class="text-center">
-        <img alt="" src="http://www.informea.org/wp-content/uploads/images/treaty/carpathian_logo.jpg">
-      </td><!-- .text-center -->
-      <td>
-        <h4>
-          <a href="#">Basel Convention on the Control of Transboundary Movements of Hazardous Wastes and Their Disposal</a>
-        </h4>
-        <ul class="list-inline small">
-          <li>
-            <strong>Basel Convention</strong>
-          </li>
-          <li class="text-muted">
-            <strong>Topics:</strong>
-            Chemicals and Waste Management, Wastes including harzadous wastes
-          </li>
-        </ul><!-- .list-inline .small -->
-      </td>
-      <td class="text-center">Europe</td>
-      <td class="text-center">N/A</td>
-    </tr>
-    <tr>
-      <td class="text-center">
-        <img alt="" src="http://www.informea.org/wp-content/uploads/images/treaty/logo_unece.png">
-      </td><!-- .text-center -->
-      <td>
-        <h4>
-          <a href="#">Convention on Access to Information, Public Participation in Decision-Making and Access to Justice in Environmental Matters</a>
-        </h4>
-        <ul class="list-inline small">
-          <li>
-            <strong>Aarhus Convention</strong>
-          </li>
-          <li class="text-muted">
-            <strong>Topics:</strong>
-            Access to Information
-          </li>
-          <li>
-            <a class="collapsed" data-toggle="collapse" href="#mycollapse">Toggle protocols</a>
-          </li>
-        </ul><!-- .list-inline .small -->
-        <ul class="list-group collapse" id="mycollapse">
-          <li class="list-group-item">
-            <a href="#">Cras justo odio</a>
-          </li>
-          <li class="list-group-item">
-            <a href="#">Dapibus ac facilisis in</a>
-          </li>
-          <li class="list-group-item">
-            <a href="#">Morbi leo risus</a>
-          </li>
-          <li class="list-group-item">
-            <a href="#">Porta ac consectetur ac</a>
-          </li>
-          <li class="list-group-item">
-            <a href="#">Vestibulum at eros</a>
-          </li>
-        </ul><!-- .list-group -->
-      </td>
-      <td class="text-center">Europe, North America</td>
-      <td class="text-center">1998</td>
-    </tr>
   </tbody>
 </table><!-- .table .table-bordered #treaties-table -->
