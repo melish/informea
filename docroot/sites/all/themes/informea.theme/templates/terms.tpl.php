0cb442e4004f888f03df2420adc7d11b1009248c
Cătălin Dogaru <catalin.dogaru@gmail.com>
2014-11-18 17:52:15 +0200

    Added the glossary page template

---
+++ docroot/sites/all/themes/informea.theme/templates/terms.tpl.php
@@ -0,0 +1,11 @@
+<div class="row">
+  <div class="col-md-6">
+    <button class="btn btn-default pull-right" type="button"><?php print t('Expand all'); ?></button>
+    <h3><?php print t('Substantive terms'); ?></h3>
+  </div>
+  <div class="col-md-6">
+    <button class="btn btn-default pull-right" type="button"><?php print t('Expand all'); ?></button>
+    <h3><?php print t('Generic terms'); ?></h3>
+  </div>
+</div>
+<p><?php print t('The InforMEA glossary was developed with the MEA Secretariats represented in the MEA Information and Knowledge Management (IKM) Initiative. Over the coming months, the InforMEA glossary will be upgraded to a Thesaurus on Environmental Law and Conventions in cooperation with partners such as FAO, IUCN, The European Environment Agency.'); ?></p>
