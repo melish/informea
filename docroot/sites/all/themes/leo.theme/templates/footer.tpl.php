19b1a0ae7689a12e04fd87faa7509ec81c6cf5b5
Cătălin Dogaru <catalin.dogaru@gmail.com>
2014-12-02 17:32:51 +0200

    Updated the footer

--- docroot/sites/all/themes/leo.theme/templates/footer.tpl.php
+++ docroot/sites/all/themes/leo.theme/templates/footer.tpl.php
@@ -4,30 +4,26 @@
  * footer.tpl.php
  */
 ?>
-<footer class="footer">
-  <div class="container">
-    <ul class="list-inline" id="footer-list">
-      <li><a href="<?php print url('<front>'); ?>"><?php print theme('image', array('path' => $directory . '/img/logo-footer.jpg')); ?></a></li>
-      <li class="hidden-xs hidden-sm">
-        <?php print t('Copyright &copy; United Nations Environment Programme'); ?>
-        |
-        <?php print l(t('Privacy'), NULL); ?>
-        |
-        <?php print l(t('Terms and Conditions'), NULL); ?>
-      </li><!-- .hidden-xs .hidden-sm -->
-      <li class="hidden-xs hidden-sm">
-        <?php print l(t('Contacts'), NULL); ?>
-        |
-        <?php print l(t('Site Locator'), NULL); ?>
-        |
-        <?php print l(t('UNEP Intranet'), NULL); ?>
-      </li><!-- .hidden-xs .hidden-sm -->
-      <li id="footer-social-media">
-        <?php print t('Follow UNEP:'); ?>
-        <a href="<?php print url('http://www.facebook.com/pages/UNEP/287683225711'); ?>" target="_blank"><?php print theme('image', array('path' => $directory . '/img/icon-facebook.gif')); ?></a>
-        <a href="<?php print url('http://twitter.com/unep'); ?>" target="_blank"><?php print theme('image', array('path' => $directory . '/img/icon-twitter.gif')); ?></a>
-        <a href="<?php print url('rss'); ?>"><?php print theme('image', array('path' => $directory . '/img/icon-rss.gif')); ?></a>
-      </li><!-- #footer-social-media -->
-    </ul><!-- .list-inline #footer-list -->
-  </div><!-- .container -->
-</footer><!-- .footer -->
+<footer id="footer" class="clearfix">
+  <ul id="footer-links" class="list-inline">
+    <li><a href="<?php print url('<front>'); ?>"><?php print theme('image', array('path' => $directory . '/img/logo-footer.jpg')); ?></a></li>
+    <li class="hidden-xs hidden-sm">
+      <?php print t('Copyright &copy; United Nations Environment Programme'); ?> |
+      <?php print l(t('Privacy'), NULL); ?> |
+      <?php print l(t('Terms and Conditions'), NULL); ?>
+    </li><!-- .hidden-xs .hidden-sm -->
+    <li class="hidden-xs hidden-sm">
+      <?php print l(t('Contacts'), NULL); ?> |
+      <?php print l(t('Site Locator'), NULL); ?> |
+      <?php print l(t('UNEP Intranet'), NULL); ?>
+    </li><!-- .hidden-xs .hidden-sm -->
+  </ul><!-- #footer-list .list-inline -->
+  <ul id="footer-social-media" class="list-inline">
+    <li>
+      <strong><?php print t('Follow UNEP:'); ?></strong>
+      <a href="<?php print url('http://www.facebook.com/pages/UNEP/287683225711'); ?>" target="_blank"><?php print theme('image', array('path' => $directory . '/img/icon-facebook.gif')); ?></a>
+      <a href="<?php print url('http://twitter.com/unep'); ?>" target="_blank"><?php print theme('image', array('path' => $directory . '/img/icon-twitter.gif')); ?></a>
+      <a href="<?php print url('rss'); ?>"><?php print theme('image', array('path' => $directory . '/img/icon-rss.gif')); ?></a>
+    </li>
+  </ul><!-- #footer-social-media .list-inline -->
+</footer><!-- #footer .clearfix -->
