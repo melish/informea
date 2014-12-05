e821b0baccae1086f6983afed4b9c674c17c608f
Cătălin Dogaru <catalin.dogaru@gmail.com>
2014-12-06 01:04:21 +0200

    Added the country side navigation

--- docroot/sites/all/themes/leo.theme/templates/country.tpl.php
+++ docroot/sites/all/themes/leo.theme/templates/country.tpl.php
@@ -13,5 +13,32 @@
     </div><!-- .well -->
   </div><!-- .col-sm-9 -->
   <div class="col-sm-3">
+    <div class="list-group">
+      <a href="#" class="list-group-item"><?php print t('Map'); ?></a>
+      <a href="#" class="list-group-item">
+        <span class="badge">56</span>
+        <?php print t('Membership'); ?>
+      </a><!-- .list-group-item -->
+      <a href="#" class="list-group-item active">
+        <span class="badge">560</span>
+        <?php print t('Legislation'); ?>
+      </a><!-- .list-group-item .active -->
+      <a href="#" class="list-group-item">
+        <span class="badge">32</span>
+        <?php print t('Decisions'); ?>
+      </a><!-- .list-group-item -->
+      <a href="#" class="list-group-item">
+        <span class="badge">320</span>
+        <?php print t('Terms'); ?>
+      </a><!-- .list-group-item -->
+      <a href="#" class="list-group-item">
+        <span class="badge">10</span>
+        <?php print t('Goals'); ?>
+      </a><!-- .list-group-item -->
+      <a href="#" class="list-group-item">
+        <span class="badge">14</span>
+        <?php print t('Publications'); ?>
+      </a><!-- .list-group-item -->
+    </div><!-- .list-group -->
   </div><!-- .col-sm-3 -->
 </div><!-- .row -->
