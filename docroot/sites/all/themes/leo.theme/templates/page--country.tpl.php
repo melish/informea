7aed0db6e56e47c8ce7aaebf230f53f6c8b79c25
Cătălin Dogaru <catalin.dogaru@gmail.com>
2015-01-13 13:05:34 +0200

    [refs #450] Added the downloads modal

--- docroot/sites/all/themes/leo.theme/templates/page--country.tpl.php
+++ docroot/sites/all/themes/leo.theme/templates/page--country.tpl.php
@@ -129,3 +129,19 @@
   </div><!-- .row -->
   <?php include 'footer.tpl.php'; ?>
 </div><!-- .container -->
+<div class="modal fade" id="modal-downloads" tabindex="-1" role="dialog" aria-labelledby="modal-downloads-label" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="<?php print t('Close'); ?>"><span aria-hidden="true"><?php print t('&times;'); ?></span></button>
+        <h4 class="modal-title" id="modal-downloads-label"><?php print t('Downloads'); ?></h4>
+      </div><!-- .modal-header -->
+      <div class="modal-body">
+        <div class="alert alert-warning" role="alert"><?php print t('<strong>Warning!</strong> This section is under development.'); ?></div>
+      </div><!-- .modal-body -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal"><?php print t('Close'); ?></button>
+      </div><!-- .modal-footer -->
+    </div><!-- .modal-content -->
+  </div><!-- .modal-dialog -->
+</div><!-- .modal.fade #modal-downloads -->
