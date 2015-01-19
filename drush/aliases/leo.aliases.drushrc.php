1c47d073b23546aae7a7b8f4bb6173ed62521a50
Cristian Romanescu <cristian@romanescu.ro>
2015-01-19 15:09:33 +0200

    Added simulate to aliases

--- drush/aliases/leo.aliases.drushrc.php
+++ drush/aliases/leo.aliases.drushrc.php
@@ -1,9 +1,17 @@
 <?php
 $aliases['dev'] = array(
-    'uri' => 'http://leo.edw.ro',
-    'root' => '--fill-in-local-file--',
-    'remote-host' => 'rom.edw.ro',
-    'remote-user' => 'cristiroma',
+  'uri' => 'http://leo.edw.ro',
+  'root' => '--fill-in-local-file--',
+  'remote-host' => 'rom.edw.ro',
+  'remote-user' => 'cristiroma',
+  'command-specific' => array (
+    'sql-sync' => array (
+      'simulate' => '1',
+    ),
+    'rsync' => array (
+      'simulate' => '0',
+    ),
+  ),
 );
 
 // Add your local aliases.
