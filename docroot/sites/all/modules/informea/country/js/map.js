d6d4c0bc85cbbfb030e89737716872fb6bb72656
Cristian Romanescu <cristian@romanescu.ro>
2015-01-21 17:46:41 +0200

    refs #450: Added country sites on map

--- docroot/sites/all/modules/informea/country/js/map.js
+++ docroot/sites/all/modules/informea/country/js/map.js
@@ -6,5 +6,14 @@ function initialize() {
     };
     console.log(Drupal.settings.map.zoom);
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var sites = Drupal.settings.map.sites;
+    for (idx = 0; idx < sites.length; idx++) {
+        var ob = sites[idx];
+        new google.maps.Marker({
+            position: new google.maps.LatLng(ob.field_latitude.und[0].value, ob.field_longitude.und[0].value),
+            map: map,
+            title: ob.title
+        });
+    }
 }
 google.maps.event.addDomListener(window, 'load', initialize);
