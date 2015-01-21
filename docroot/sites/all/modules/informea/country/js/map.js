0a957cc388d2e09b64951cdba52e352ee15fbc1d
Cristian Romanescu <cristian@romanescu.ro>
2015-01-21 17:31:09 +0200

    Country profile map pans and zooms on country

--- docroot/sites/all/modules/informea/country/js/map.js
+++ docroot/sites/all/modules/informea/country/js/map.js
@@ -1,11 +1,10 @@
 var map;
 function initialize() {
     var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644)
+        zoom: parseInt(Drupal.settings.map.zoom),
+        center: new google.maps.LatLng(Drupal.settings.map.latitude, Drupal.settings.map.longitude)
     };
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+    console.log(Drupal.settings.map.zoom);
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
-
 google.maps.event.addDomListener(window, 'load', initialize);
