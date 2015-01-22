d88b743dc6bd7ccba653224b2e68942e8b5ad2ce
Cristian Romanescu <cristian@romanescu.ro>
2015-01-22 10:04:29 +0200

    Fixed map markers to differentiate WHC/Ramsar

--- docroot/sites/all/modules/informea/country/js/map.js
+++ docroot/sites/all/modules/informea/country/js/map.js
@@ -1,10 +1,22 @@
 var map;
+
 function initialize() {
+    var pinW = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=w|FE7569",
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0,0),
+        new google.maps.Point(10, 34));
+    var pinR = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=r|5e9ddb",
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0,0),
+        new google.maps.Point(10, 34));
+    var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+        new google.maps.Size(40, 37),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(12, 35));
     var mapOptions = {
         zoom: parseInt(Drupal.settings.map.zoom),
         center: new google.maps.LatLng(Drupal.settings.map.latitude, Drupal.settings.map.longitude)
     };
-    console.log(Drupal.settings.map.zoom);
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     var sites = Drupal.settings.map.sites;
     for (idx = 0; idx < sites.length; idx++) {
@@ -12,7 +24,9 @@ function initialize() {
         new google.maps.Marker({
             position: new google.maps.LatLng(ob.field_latitude.und[0].value, ob.field_longitude.und[0].value),
             map: map,
-            title: ob.title
+            title: ob.title,
+            icon: ob.field_treaty.und[0].target_id == "267" ? pinW : pinR, // 267 - WHC
+            shadow: pinShadow
         });
     }
 }
