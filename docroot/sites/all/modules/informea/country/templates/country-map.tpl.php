9b7c5c58bc30e8a6781e4ba8156b0a2e8712937f
Cătălin Dogaru <catalin.dogaru@gmail.com>
2015-01-23 16:33:48 +0200

    [refs #450] Added map styles

--- docroot/sites/all/modules/informea/country/templates/country-map.tpl.php
+++ docroot/sites/all/modules/informea/country/templates/country-map.tpl.php
@@ -1,12 +1,12 @@
-<div id="map-canvas" style="height: 400px;margin: 0;padding: 0"></div>
+<div id="map-canvas"></div>
 <h3><?php print t('Legend'); ?></h3>
 <ul class="list-inline">
-	<li>
-		<img src="http://chart.apis.google.com/chart?chst=d_map_pin_letter&amp;chld=w|FE7569">
-		<?php print t('Ramsar site'); ?>
-	</li>
-	<li>
-		<img src="http://chart.apis.google.com/chart?chst=d_map_pin_letter&amp;chld=r|5e9ddb">
-		<?php print t('WHC site'); ?>
-	</li>
+  <li>
+    <img src="http://chart.apis.google.com/chart?chst=d_map_pin_letter&amp;chld=w|FE7569">
+    <?php print t('Ramsar site'); ?>
+  </li>
+  <li>
+    <img src="http://chart.apis.google.com/chart?chst=d_map_pin_letter&amp;chld=r|5e9ddb">
+    <?php print t('WHC site'); ?>
+  </li>
 </ul>
