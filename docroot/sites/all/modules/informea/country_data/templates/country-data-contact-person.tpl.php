4ef062d27b2e9738907fbf048eaac9bea082bc76
Cristian Romanescu <cristian@romanescu.ro>
2015-01-23 17:32:20 +0200

    Moved focal point rendering to templates

---
+++ docroot/sites/all/modules/informea/country_data/templates/country-data-contact-person.tpl.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @file
+ * country-data-contact-person.tpl.php
+ */
+?>
+<?php
+  $wrapper = entity_metadata_wrapper('node', $contact_person);
+  $position = $wrapper->field_person_position->value();
+  $department = $wrapper->field_person_department->value();
+  $institution = $wrapper->field_person_department->value();
+  $address = $wrapper->field_address->value();
+  $fax = $wrapper->field_contact_fax->value();
+  $mail = $wrapper->field_person_email->value();
+  $mail_link = FALSE;
+  if ($mail) {
+    $mail_url  = recaptcha_mailhide_url( variable_get( 'recaptcha_public' ), variable_get( 'recaptcha_private' ), $mail );
+    $mail_part = implode( '&hellip;@', _recaptcha_mailhide_email_parts( $wrapper->field_person_email->value() ) );
+    $mail_link = l($mail_part, $mail_url, array(
+      'attributes' => array(
+        'onclick' => sprintf('window.open(\'%s\', \'\', \'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300\'); return false;', $mail_url)
+      ), 'external' => TRUE, 'html' => TRUE
+    ));
+  }
+?>
+<h4 class="list-group-item-heading">
+	<span class="glyphicon glyphicon-user"></span> <?php print $wrapper->label(); ?>
+</h4>
+<dl class="dl-horizontal">
+<?php if ($position): ?>
+	<dt><?php print t('Position'); ?></dt>
+	<dd><?php print $position; ?></dd>
+<?php endif; ?>
+
+<?php if ($department): ?>
+	<dt><?php print t('Department'); ?></dt>
+	<dd><?php print $department; ?></dd>
+<?php endif; ?>
+
+<?php if ($institution): ?>
+	<dt><?php print t('Institution'); ?></dt>
+	<dd><?php print $institution; ?></dd>
+<?php endif; ?>
+
+<?php if ($address): ?>
+	<dt><?php print t('Address'); ?></dt>
+	<dd><?php print $address; ?></dd>
+<?php endif; ?>
+
+<?php if ($fax): ?>
+		<dt><?php print t('Fax'); ?></dt>
+	<dd><?php print $fax; ?></dd>
+<?php endif; ?>
+
+<?php if ($mail_link): ?>
+	<dt><?php print t('E-Mail'); ?></dt>
+	<dd><?php print $mail_link; ?></dd>
+<?php endif; ?>
+</dl>
