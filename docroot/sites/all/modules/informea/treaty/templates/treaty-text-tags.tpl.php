d9dc68a3fdf14c3151b70fb10c65aad902326148
Cristian Romanescu <cristian@romanescu.ro>
2015-01-23 08:52:22 +0200

    Fixed comma after last tagged term

--- docroot/sites/all/modules/informea/treaty/templates/treaty-text-tags.tpl.php
+++ docroot/sites/all/modules/informea/treaty/templates/treaty-text-tags.tpl.php
@@ -9,7 +9,7 @@
   Tagged Terms: &ensp;
   <?php for($i = 0; $i < count($tags); $i++): ?>
     <?php $tw = entity_metadata_wrapper('taxonomy_term', $tags[$i]); ?>
-    <a href="#" target="_blank"><?php print $tw->label(); ?></a><?php if ($i < count($tags)) { echo ', '; } ?>
+    <a href="#" target="_blank"><?php print $tw->label(); ?></a><?php if ($i < count($tags) - 1) { echo ', '; } ?>
   <?php endfor; ?>
  </span>
 <?php endif; ?>
