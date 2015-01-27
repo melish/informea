20873425108816dbefbb8326078303e070f00dc2
Cătălin Dogaru <catalin.dogaru@gmail.com>
2015-01-27 18:10:26 +0200

    [refs #450] Updated treaty page

--- docroot/sites/all/modules/informea/treaty/templates/treaty-text-article.tpl.php
+++ docroot/sites/all/modules/informea/treaty/templates/treaty-text-article.tpl.php
@@ -5,35 +5,30 @@
  */
 ?>
 <?php if (isset($article)): ?>
-<?php
+  <?php
   $aw = entity_metadata_wrapper('node', $article);
   $tags = $aw->field_informea_tags->value();
-?>
-<div class="panel panel-default">
-  <div class="panel-heading smallipop">
-    <h4 class="panel-title">
-      <a data-toggle="collapse" href="#article_<?php echo $article->nid; ?>">
+  ?>
+  <div class="panel panel-default">
+    <div class="panel-heading smallipop collapsed" role="tab" id="heading-<?php echo $article->nid; ?>" data-toggle="collapse" data-parent="#treaty-text" data-target="#article-<?php echo $article->nid; ?>" aria-expanded="false" aria-controls="article-<?php echo $article->nid; ?>">
+      <h4 class="panel-title">
         <?php echo $article->official_title; ?>
-        <a target="_blank" class="permalink pull-right" href="#"><i class="glyphicon glyphicon-link"></i></a>
-      </a>
-    </h4>
-    <?php print theme('treaty_text_tags', array('tags' => $tags)); ?>
-  </div>
-  <div id="article_<?php echo $article->nid; ?>" class="panel-collapse collapse">
-    <div class="panel-body">
-      <?php if(!empty($article->paragraphs)): ?>
-        <?php foreach ($article->paragraphs as $paragraph): ?>
-          <?php print theme('treaty_text_paragraph', array('paragraph' => $paragraph, 'article' => $article)); ?>
-        <?php endforeach; ?>
-      <?php else: ?>
-        <div class="article tagged-content content">
-        <?php
-          $body = field_view_field('node', $article, 'body', 'teaser');
-          print drupal_render($body);
-        ?>
-        </div><!-- .article -->
-      <?php endif; ?>
-    </div><!-- .panel-body -->
-  </div><!-- .panel-collapse -->
-</div><!-- .panel -->
+        <a target="_blank" class="permalink pull-right" href="#article-<?php echo $article->nid; ?>"><i class="glyphicon glyphicon-link"></i></a>
+      </h4><!-- .panel-title -->
+      <?php print theme('treaty_text_tags', array('tags' => $tags)); ?>
+    </div><!-- .panel-heading .smallipop -->
+    <div id="article-<?php echo $article->nid; ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-<?php echo $article->nid; ?>">
+      <div class="panel-body">
+        <?php if (!empty($article->paragraphs)): ?>
+          <?php foreach ($article->paragraphs as $paragraph): ?>
+            <?php print theme('treaty_text_paragraph', array('paragraph' => $paragraph, 'article' => $article)); ?>
+          <?php endforeach; ?>
+        <?php else: ?>
+          <div class="article tagged-content content">
+            <?php print drupal_render(field_view_field('node', $article, 'body', 'teaser')); ?>
+          </div><!-- .article .tagged-content .content -->
+        <?php endif; ?>
+      </div><!-- .panel-body -->
+    </div><!-- .panel-collapse .collapse -->
+  </div><!-- .panel .panel-default -->
 <?php endif; ?>
--- docroot/sites/all/modules/informea/treaty/templates/treaty-text.tpl.php
+++ docroot/sites/all/modules/informea/treaty/templates/treaty-text.tpl.php
@@ -5,9 +5,9 @@
  */
 ?>
 <?php if (isset($articles) && is_array($articles)): ?>
-<div class="panel-group tagged-content" id="treaty-text">
-<?php foreach ($articles as $article) : ?>
-  <?php print theme('treaty_text_article', array('article' => $article)); ?>
-<?php endforeach; ?>
-</div>
+  <div class="panel-group tagged-content" id="treaty-text" role="tablist" aria-multiselectable="true">
+    <?php foreach ($articles as $article) : ?>
+      <?php print theme('treaty_text_article', array('article' => $article)); ?>
+    <?php endforeach; ?>
+  </div>
 <?php endif; ?>
