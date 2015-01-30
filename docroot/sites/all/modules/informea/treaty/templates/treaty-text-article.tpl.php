35651b171b7e2885a0131b8ed89461ebb8c6ebbc
Cătălin Dogaru <catalin.dogaru@gmail.com>
2015-01-30 16:16:54 +0200

    Updated treaty article template

--- docroot/sites/all/modules/informea/treaty/templates/treaty-text-article.tpl.php
+++ docroot/sites/all/modules/informea/treaty/templates/treaty-text-article.tpl.php
@@ -9,7 +9,6 @@
   $aw = entity_metadata_wrapper('node', $article);
   $tags = $aw->field_informea_tags->value();
   $expanded = isset($_GET['article']) ? $_GET['article'] == $article->nid : FALSE;
-  $highlight = $expanded && !isset($_GET['paragraph']) ? ' highlight' : '';
   ?>
   <div class="panel <?php print $expanded ? 'panel-warning' : 'panel-default'; ?>">
     <div class="panel-heading smallipop<?php print $expanded ? '' : ' collapsed' ;?>" role="tab" id="heading-<?php echo $article->nid; ?>" data-toggle="collapse" data-parent="#treaty-text" data-target="#article-<?php echo $article->nid; ?>" aria-expanded="<?php print $expanded ? 'true' : 'false' ;?>" aria-controls="article-<?php echo $article->nid; ?>">
@@ -57,8 +56,8 @@
       </h4><!-- .panel-title -->
       <?php print theme('treaty_text_tags', array('tags' => $tags)); ?>
     </div><!-- .panel-heading .smallipop -->
-    <div id="article-<?php echo $article->nid; ?>" class="panel-collapse collapse<?php print $expanded ? ' in' . $highlight : '' ;?>" role="tabpanel" aria-labelledby="heading-<?php echo $article->nid; ?>">
-      <div class="panel-body">
+    <div id="article-<?php echo $article->nid; ?>" class="panel-collapse collapse<?php print $expanded ? ' in' : '' ;?>" role="tabpanel" aria-labelledby="heading-<?php echo $article->nid; ?>">
+      <div class="article<?php print $expanded && !isset($_GET['paragraph']) ? ' highlight' : ''; ?>">
         <?php if (!empty($article->paragraphs)): ?>
           <?php foreach ($article->paragraphs as $paragraph): ?>
             <?php print theme('treaty_text_paragraph', array('paragraph' => $paragraph, 'article' => $article)); ?>
@@ -68,20 +67,7 @@
             <?php $f = field_view_field('node', $article, 'body', 'teaser'); print drupal_render($f); ?>
           </div><!-- .article .tagged-content .content -->
         <?php endif; ?>
-        <?php if (user_access('administer nodes')): ?>
-          <div class="text-right">
-            <?php
-            print l('<i class="glyphicon glyphicon-plus"></i> ' . t('Add paragraph'), 'node/add/treaty-paragraph', array(
-              'html' => TRUE,
-              'query' => array('edit' => array(
-                'field_parent_treaty_article' => array('und' => $article->nid),
-                'field_treaty' => array('und' => $aw->field_treaty->value()[0]->nid)
-              ))
-            ));
-            ?>
-          </div><!-- .text-right -->
-        <?php endif; ?>
-      </div><!-- .panel-body -->
+      </div><!-- .article -->
     </div><!-- .panel-collapse .collapse -->
   </div><!-- .panel .panel-default -->
 <?php endif; ?>
