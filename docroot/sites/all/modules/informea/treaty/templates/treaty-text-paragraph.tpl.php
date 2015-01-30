bb8a4a9d072a99290fbd80b3cc1e4a05470b87f5
Cătălin Dogaru <catalin.dogaru@gmail.com>
2015-01-30 16:17:18 +0200

    Updated treaty paragraph template

--- docroot/sites/all/modules/informea/treaty/templates/treaty-text-paragraph.tpl.php
+++ docroot/sites/all/modules/informea/treaty/templates/treaty-text-paragraph.tpl.php
@@ -10,20 +10,28 @@ $tags = $pw->field_informea_tags->value();
 $indent = $pw->field_paragraph_indentation->value();
 $highlight = isset($_GET['paragraph']) ? $_GET['paragraph'] == $paragraph->nid : FALSE;
 ?>
-<div class="paragraph indent-<?php print $indent; ?><?php print $highlight ? ' highlight' : '' ;?>" id="paragraph-<?php print $paragraph->nid; ?>">
-  <p class="smallipop">
-    <span class="pull-right">
+<div class="paragraph smallipop indent-<?php print $indent; ?><?php print $highlight ? ' highlight' : '' ;?>" id="paragraph-<?php print $paragraph->nid; ?>">
+  <p>
+    <?php $body = field_view_field('node', $paragraph, 'body', 'teaser'); ?>
+    <?php print strip_tags($body[0]['#markup']); // calling render adds unwanted div's @todo ?>
+  </p>
+  <?php print theme('treaty_text_tags', array('tags' => $tags)); ?>
+  <ul class="list-inline actions">
+    <?php if (!empty($tags) && is_array($tags)): ?>
+      <li>
+        <span class="glyphicon glyphicon-tag"></span>
+        <?php print t('Tagged terms'); ?>
+      </li>
+    <?php endif; ?>
+    <li>
       <?php if (user_access('administer nodes')): ?>
-        <?php
-        print l('<i class="glyphicon glyphicon-pencil"></i>', 'node/' . $paragraph->nid . '/edit', array(
-          'attributes' => array('class' => array('permalink')),
-          'html' => TRUE
-        ));
-        ?>
+        <?php print l('<span class="glyphicon glyphicon-pencil"></span> ' . t('Edit paragraph'), 'node/' . $paragraph->nid . '/edit', array('html' => TRUE)); ?>
       <?php endif; ?>
+    </li>
+    <li>
       <?php
-      print l('<i class="glyphicon glyphicon-link"></i>', 'treaties/cbd', array(
-        'attributes' => array('class' => array('permalink'), 'target' => '_blank'),
+      print l('<span class="glyphicon glyphicon-link"></span> ' . t('Permalink'), 'treaties/cbd', array(
+        'attributes' => array('target' => '_blank'),
         'fragment' => 'paragraph-' . $paragraph->nid,
         'html' => TRUE,
         'query' => array(
@@ -32,12 +40,6 @@ $highlight = isset($_GET['paragraph']) ? $_GET['paragraph'] == $paragraph->nid :
         )
       ));
       ?>
-    </span>
-    <?php
-    $body = field_view_field('node', $paragraph, 'body', 'teaser');
-
-    print strip_tags($body[0]['#markup']); // calling render adds unwanted div's @todo
-    ?>
-    <?php print theme('treaty_text_tags', array('tags' => $tags)); ?>
-  </p><!-- .smallipop -->
-</div><!-- .paragraph -->
+    </li>
+  </ul><!-- .list-inline .actions -->
+</div><!-- .paragraph .smallipop -->
