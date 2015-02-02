58a184ca9ec6d81655ed1b0feaa27059642e5af2
Cătălin Dogaru <catalin.dogaru@gmail.com>
2015-02-02 18:14:04 +0200

    Updated treaty decisions to open in modal

--- docroot/sites/all/modules/informea/treaty/templates/treaty-decisions.tpl.php
+++ docroot/sites/all/modules/informea/treaty/templates/treaty-decisions.tpl.php
@@ -12,7 +12,7 @@
           <?php $w = entity_metadata_wrapper('node', $decision); ?>
           <?php print $w->field_decision_number->value(); ?>
         </td><!-- .col-sm-1 -->
-        <td><?php print l($w->label(), 'node/' . $w->getIdentifier()); ?></td>
+        <td><?php print l($w->label(), 'node/' . $w->getIdentifier() . '/ajax', array('attributes' => array('data-toggle' => 'modal', 'data-target' => '#modal-decision'))); ?></td>
       </tr>
     <?php endforeach; ?>
   </tbody>
