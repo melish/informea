26a7b5283ca7129ebab09af1bde2ca5aa9188ac4
Cătălin Dogaru <catalin.dogaru@gmail.com>
2015-02-03 15:12:32 +0200

    Updated treaty decisions template

--- docroot/sites/all/modules/informea/treaty/templates/treaty-decisions.tpl.php
+++ docroot/sites/all/modules/informea/treaty/templates/treaty-decisions.tpl.php
@@ -6,13 +6,21 @@
 ?>
 <table class="table table-hover">
   <tbody>
-    <?php foreach ($decisions as $id => $decision): ?>
+    <?php foreach ($decisions as $decision): ?>
+      <?php $wrapper = entity_metadata_wrapper('node', $decision); ?>
       <tr>
-        <td class="col-sm-1">
-          <?php $w = entity_metadata_wrapper('node', $decision); ?>
-          <?php print $w->field_decision_number->value(); ?>
-        </td><!-- .col-sm-1 -->
-        <td><?php print l($w->label(), 'node/' . $w->getIdentifier() . '/ajax', array('attributes' => array('data-toggle' => 'modal', 'data-target' => '#modal-decision'))); ?></td>
+        <td><?php print $wrapper->field_decision_number->value(); ?></td>
+        <td>
+          <?php
+          print l($wrapper->label(), 'node/' . $wrapper->getIdentifier(), array(
+            'attributes' => array(
+              'data-toggle' => 'modal',
+              'data-target' => '#modal-decision',
+              'data-remote' => url('node/' . $wrapper->getIdentifier() . '/ajax')
+            )
+          ));
+          ?>
+        </td>
       </tr>
     <?php endforeach; ?>
   </tbody>
