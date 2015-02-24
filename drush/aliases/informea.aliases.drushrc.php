4dee0e93e3694d9c4b0b5c163229b70635c366d6
Cristian Romanescu <cristian@romanescu.ro>
2015-02-24 08:52:05 +0200

    Added aliases or development

---
+++ drush/aliases/informea.aliases.drushrc.php
@@ -0,0 +1,12 @@
+<?php
+$aliases['dev'] = array(
+  'uri' => 'http://informea.edw.ro',
+  'root' => '/var/www/html/informea/informea.edw.ro/project/docroot',
+  'remote-host' => 'php-prod1.edw.lan',
+  'remote-user' => 'php',
+);
+
+// Add your local aliases.
+if (file_exists(dirname(__FILE__) . '/aliases.local.php')) {
+  include dirname(__FILE__) . '/aliases.local.php';
+}
