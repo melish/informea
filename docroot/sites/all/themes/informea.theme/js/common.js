e06e4b960b2cd3daec36746e1dc1d684b1146f5b
Cătălin Dogaru <catalin.dogaru@gmail.com>
2015-06-10 16:27:14 +0300

    Prevent panels from collapse on cliking a permalink

--- docroot/sites/all/themes/informea.theme/js/common.js
+++ docroot/sites/all/themes/informea.theme/js/common.js
@@ -6,4 +6,8 @@ jQuery(document).ready(function ($) {
   $('#dialog-modal-ajax').on('hidden.bs.modal', function () {
     $(this).removeData('bs.modal');
   });
+
+  $('.permalink').click(function (event) {
+  	event.stopPropagation();
+  });
 });
