f5246bd1d42166b64ddc2bb95f6525a8b39116c9
Cristian Romanescu <cristian@romanescu.ro>
2015-06-27 12:22:34 +0300

    Added production drush alias

--- drush/aliases/informea.aliases.drushrc.php
+++ drush/aliases/informea.aliases.drushrc.php
@@ -1,4 +1,8 @@
 <?php
+$aliases['prod'] = array(
+  'uri' => 'http://www.informea.org',
+);
+
 $aliases['dev'] = array(
   'uri' => 'http://informea.edw.ro',
   'root' => '/var/www/html/informea/informea.edw.ro/project/docroot',
