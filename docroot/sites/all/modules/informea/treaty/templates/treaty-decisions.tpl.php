5fca80ef2398764d5d51610bfc326b3ca9fb81dd
Cristian Romanescu <cristian@romanescu.ro>
2015-07-03 02:08:08 +0300

    Optimizations to the decision view

--- docroot/sites/all/modules/informea/treaty/templates/treaty-decisions.tpl.php
+++ docroot/sites/all/modules/informea/treaty/templates/treaty-decisions.tpl.php
@@ -7,20 +7,24 @@
 <?php
 /** @var array $variables */
 $decisions = $variables['decisions'];
+global $language;
 ?>
 <table class="table table-hover">
   <tbody>
-    <?php foreach ($decisions as $decision): ?>
-      <?php $wrapper = entity_metadata_wrapper('node', $decision);?>
-      <tr id="decision-<?php print $decision->nid ?>">
-        <td class="col-sm-1 text-center"><?php print $wrapper->field_decision_number->value(); ?></td>
+  <?php
+    foreach ($decisions as $nid => $decision):
+        $number = !empty($decision->field_decision_number[LANGUAGE_NONE][0]['value']) ? $decision->field_decision_number[LANGUAGE_NONE][0]['value'] : '&nbsp;';
+        $title = !empty($decision->title_field[$language->language][0]['value']) ? $decision->title_field[$language->language][0]['value'] : '&nbsp;';
+  ?>
+      <tr id="decision-<?php print $nid ?>">
+        <td class="col-sm-1 text-center"><?php print $number; ?></td>
         <td>
           <?php
-          print l($wrapper->label(), 'node/' . $wrapper->getIdentifier(), array(
+          print l($title, 'node/' . $nid, array(
             'attributes' => array(
               'data-toggle' => 'modal',
               'data-target' => '#dialog-modal-ajax',
-              'data-remote' => url('ajax/modal/node/' . $wrapper->getIdentifier())
+              'data-remote' => url('ajax/modal/node/' . $nid)
             )
           ));
           ?>
