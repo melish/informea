49580180f24562bffd7193fc1bd72b7be06287a3
Cătălin Dogaru <catalin.dogaru@gmail.com>
2015-07-06 11:43:46 +0300

    Added block template

--- docroot/sites/all/modules/informea/treaty/templates/treaty-global.tpl.php
+++ docroot/sites/all/modules/informea/treaty/templates/treaty-global.tpl.php
@@ -4,4 +4,15 @@
  * treaty-global.tpl.php
  */
 ?>
-treaty-global.tpl.php
+<div class="text-center">
+  <?php foreach ($treaties as $group): ?>
+    <div class="brand-group">
+      <?php foreach ($group as $treaty): ?>
+        <a href="<?php print $treaty->url->value(); ?>" class="brand">
+          <div class="image"></div>
+          <?php print $treaty->label(); ?>
+        </a><!-- .brand -->
+      <?php endforeach; ?>
+    </div><!-- .brand-group -->
+  <?php endforeach; ?>
+</div><!-- .text-center -->
