b8fe4d5e8064c7db5a4555321df29e36e6acf320
Cătălin Dogaru <catalin.dogaru@gmail.com>
2015-07-06 12:01:29 +0300

    Added treaty brand class

--- docroot/sites/all/modules/informea/treaty/templates/treaty-global.tpl.php
+++ docroot/sites/all/modules/informea/treaty/templates/treaty-global.tpl.php
@@ -8,7 +8,8 @@
   <?php foreach ($treaties as $group): ?>
     <div class="brand-group">
       <?php foreach ($group as $treaty): ?>
-        <a href="<?php print $treaty->url->value(); ?>" class="brand">
+        <?php $url = $treaty->url->value(); ?>
+        <a href="<?php print $url; ?>" class="brand brand-<?php print basename($url); ?>">
           <div class="image"></div>
           <?php print $treaty->label(); ?>
         </a><!-- .brand -->
