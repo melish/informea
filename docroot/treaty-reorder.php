3227619ab6607eb112238fa9f7888dc24d0bc10d
Cătălin Dogaru <catalin.dogaru@gmail.com>
2015-07-14 19:06:56 +0300

    Added migrate script for treaty article paragraph weights

--- docroot/treaty-reorder.php
+++ docroot/treaty-reorder.php
@@ -1,4 +1,6 @@
 <?php
+// Reorder treaty articles
+
 $query = new EntityFieldQuery();
 
 $query->entityCondition('entity_type', 'node')
@@ -39,6 +41,58 @@ if (isset($result['node'])) {
           'view_name' => 'treaties_reorder',
           'view_display' => 'page_articles',
           'args' => sprintf('{"field_treaty_target_id":"%d"}', $treaty_id),
+          'entity_id' => $nid,
+          'weight' => $weight,
+          'parent' => 0
+        ))
+        ->execute();
+    }
+  }
+}
+
+// Reorder treaty article paragraphs
+
+$query = new EntityFieldQuery();
+
+$query->entityCondition('entity_type', 'node')
+  ->entityCondition('bundle', 'treaty_paragraph')
+  ->propertyCondition('status', NODE_PUBLISHED);
+
+$result = $query->execute();
+
+if (isset($result['node'])) {
+  $nids = array_keys($result['node']);
+  $nodes = entity_load('node', $nids);
+
+  foreach ($nodes as $node) {
+    $wrapper = entity_metadata_wrapper('node', $node);
+
+    $nid = $wrapper->getIdentifier();
+    $treaty_id = $wrapper->field_treaty->value()[0]->vid;
+    $article_id = $wrapper->field_parent_treaty_article->value()->vid;
+    $weight = $wrapper->field_sorting_order->value();
+
+    $query = db_select('draggableviews_structure', 'd')
+      ->condition('d.entity_id', $nid)
+      ->fields('d', array('entity_id'));
+
+    $result = $query->execute()->fetchAllKeyed();
+
+    if (!empty($result)) {
+      $entity_id = array_keys($result)[0];
+
+      db_update('draggableviews_structure')
+        ->fields(array(
+          'weight' => $weight,
+        ))
+        ->condition('entity_id', $nid)
+        ->execute();
+    } else {
+      db_insert('draggableviews_structure')
+        ->fields(array(
+          'view_name' => 'treaties_reorder',
+          'view_display' => 'page_paragraphs',
+          'args' => sprintf('{"field_parent_treaty_article_target_id":"%d","field_treaty_target_id":"%d"}', $article_id, $treaty_id),
           'entity_id' => $nid,
           'weight' => $weight,
           'parent' => 0
