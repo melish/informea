88a90a962759e44fc56e3af0f274f2ebd4de9fad
Octavian Belu <belu.octavian@yahoo.com>
2015-09-30 17:10:05 +0300

    Fixed %treaty/national-reports page bug

--- docroot/sites/all/modules/informea/country_deliverable/templates/country-deliverable-rod-cycle.tpl.php
+++ docroot/sites/all/modules/informea/country_deliverable/templates/country-deliverable-rod-cycle.tpl.php
@@ -17,25 +17,27 @@ if(count($decisions)): ?>
 <p class="decision">
 <?php
   $decision = current($decisions);
-  $dw = entity_metadata_wrapper('node', $decision);
-  $number = $dw->field_decision_number->value();
-  $number = strpos($number, '.') ? $number : $number . '.';
-  echo t('Defined by the decision <em><a href="!link" target="_blank">!number !title</a></em>',
-    array(
-      '!link' => url('node/' . $decision->nid),
-      '!number' => $number,
-      '!title' => $dw->label()));
+  if ($decision !== NULL) {
+    $dw = entity_metadata_wrapper('node', $decision);
+    $number = $dw->field_decision_number->value();
+    $number = strpos($number, '.') ? $number : $number . '.';
+    echo t('Defined by the decision <em><a href="!link" target="_blank">!number !title</a></em>',
+      array(
+        '!link' => url('node/' . $decision->nid),
+        '!number' => $number,
+        '!title' => $dw->label()));
 
-  $paragraphs = $w->field_decision_paragraph->value();
-  foreach ($paragraphs as $paragraph):
-    $paragraph->title = '';
-    $render = node_view($paragraph, 'teaser');
-    unset($render['links']['node']['#links']['node-readmore']);
-    echo drupal_render($render);
-  endforeach;
+    $paragraphs = $w->field_decision_paragraph->value();
+    foreach ($paragraphs as $paragraph):
+      $paragraph->title = '';
+      $render = node_view($paragraph, 'teaser');
+      unset($render['links']['node']['#links']['node-readmore']);
+      echo drupal_render($render);
+    endforeach;
 
-  $dfv = field_view_field('node', $decision, 'field_files');
-  echo render($dfv);
+    $dfv = field_view_field('node', $decision, 'field_files');
+    echo render($dfv);
+  }
 ?>
 </p>
 <?php endif; ?>
