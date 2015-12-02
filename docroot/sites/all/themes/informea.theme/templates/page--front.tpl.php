73854489fb469132b754d6a3e566d0126f3073f1
Cristian Romanescu <cristian@romanescu.ro>
2015-12-02 15:24:08 +0200

    Front-page blocks are dynamic, removed hover pointer: https://trello.com/c/MNIdqP4v

--- docroot/sites/all/themes/informea.theme/templates/page--front.tpl.php
+++ docroot/sites/all/themes/informea.theme/templates/page--front.tpl.php
@@ -35,49 +35,9 @@
 <div class="container">
   <?php if (!user_is_anonymous()) { print $messages; } ?>
   <div class="row" id="row-features">
-    <div class="col-md-4" id="col-terms">
-      <div class="well well-transparent well-hover text-center">
-        <h3><?php print t('Glossary'); ?></h3>
-        <p><?php print theme('image', array('path' => $directory . '/img/glossary.gif', 'attributes' => array('class' => array('img-responsive', 'center-block')))); ?></p>
-        <p class="description"><?php print t('Browse the MEA unified glossary of terms to see tagged information across multiple MEAs.'); ?></p>
-        <p><?php print l(t('Open the glossary'), 'terms'); ?></p>
-      </div><!-- .well .text-center -->
-    </div><!-- .col-md-4 #terms -->
-    <div class="col-md-4" id="col-treaties">
-      <div class="well well-hover text-center">
-        <h3><?php print t('Treaties'); ?></h3>
-        <p class="description"><?php print t('Browse more than 50 global and regional &ndash; environmental related treaties. Read texts, decisions, consult the list of parties and focal points. Print nice reports about treaties with geographical coverage maps and figures.'); ?></p>
-        <ul class="list-inline regions">
-          <li><strong><?php print t('Regions'); ?>:</strong></li>
-          <li><?php print l(t('Global'), 'treaties', array('fragment' => t('Global'))); ?></li>
-          <li><?php print l(t('Africa'), 'treaties', array('fragment' => t('Africa'))); ?></li>
-          <li><?php print l(t('Asia and the Pacific'), 'treaties', array('fragment' => t('Asia and the Pacific'))); ?></li>
-          <li><?php print l(t('Europe'), 'treaties', array('fragment' => t('Europe'))); ?></li>
-          <li><?php print l(t('Latin America and the Caribbean'), 'treaties', array('fragment' => t('Latin America and the Caribbean'))); ?></li>
-          <li><?php print l(t('North America'), 'treaties', array('fragment' => t('North America'))); ?></li>
-          <li><?php print l(t('West Asia'), 'treaties', array('fragment' => t('West Asia'))); ?></li>
-        </ul><!-- .list-inline .regions -->
-        <p><?php print l(t('Browse all treaties'), 'treaties'); ?></p>
-      </div><!-- .well .text-center -->
-    </div><!-- .col-md-4 #col-treaties -->
-    <div class="col-md-4" id="col-countries">
-      <div class="well well-transparent well-hover text-center">
-        <h3><?php print t('Countries'); ?></h3>
-        <div class="country-selector">
-          <p><?php print theme('image', array('path' => $directory . '/img/countries.gif', 'attributes' => array('class' =>  array('img-responsive', 'center-block')))); ?></p>
-          <form role="form">
-            <select class="form-control input-sm">
-              <option value=""><?php print t('Select a country&hellip;'); ?></option>
-              <?php foreach ($countries as $iso2 => $country): ?>
-                <option value="<?php print $iso2; ?>"><?php print $country; ?></option>
-              <?php endforeach; ?>
-            </select><!-- .form-control .input-sm -->
-          </form>
-        </div><!-- .country-selector -->
-        <p class="description"><?php print t('Visualise MEA related information into geographical context. Countries that are parties to treaties and protocols have focal points, national reports and action plans.'); ?></p>
-        <p><?php print l(t('See all countries'), 'countries'); ?></p>
-      </div><!-- .well .text-center -->
-    </div><!-- .col-md-4 #col-countries -->
+    <?php if (!empty($page['content'])): ?>
+      <?php print render($page['front_page_content']); ?>
+    <?php endif; ?>
   </div><!-- .row #row-features -->
 </div><!-- .container -->
 <?php include 'footer.tpl.php'; ?>
