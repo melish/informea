d61974c43b677b0e4c146eabf26fdfd229415bac
Octavian Belu <belu.octavian@yahoo.com>
2016-01-12 17:47:27 +0200

    Fixed treaties table filter

--- docroot/sites/all/modules/informea/treaty/js/treaties.js
+++ docroot/sites/all/modules/informea/treaty/js/treaties.js
@@ -1,4 +1,21 @@
 jQuery(document).ready(function ($) {
+  if (hash = window.location.hash) {
+    region = $('[data-value="' + hash.substr(1) + '"]');
+    region.parent().addClass('active').siblings().removeClass('active');
+
+    var selector = region.data('selector');
+    var target = '#table-treaties';
+    var value = region.data('value');
+    var rows = $('> tbody > tr', target).hide().filter(function () {
+      return $(selector, this).text().indexOf(value) > -1;
+    }).show().length;
+
+    $('.view-treaty-listing-page .total').html(rows);
+
+    // Collapse all protocols
+    $('> tbody > tr.active', target).hide();
+    $('.glyphicon-minus-sign', target).toggleClass('glyphicon-plus-sign glyphicon-minus-sign');
+  }
   $('[data-filter="table"]').click(function (event) {
     event.preventDefault();
 
@@ -6,7 +23,7 @@ jQuery(document).ready(function ($) {
 
     var selector = $(this).data('selector');
     var target = this.hash;
-    var value = $(this).data('value');
+    var value = window.location.hash = $(this).data('value');
     var rows = $('> tbody > tr', target).hide().filter(function () {
       return $(selector, this).text().indexOf(value) > -1;
     }).show().length;
