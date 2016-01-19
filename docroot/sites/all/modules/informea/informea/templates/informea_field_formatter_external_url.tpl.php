948d08d972d5906eb87ea88284d23cc4a1a0c3d0
Cristian Romanescu <cristian@romanescu.ro>
2016-01-19 17:37:14 +0200

    Fixed the external_url formatter to now show links to local files

--- docroot/sites/all/modules/informea/informea/templates/informea_field_formatter_external_url.tpl.php
+++ docroot/sites/all/modules/informea/informea/templates/informea_field_formatter_external_url.tpl.php
@@ -2,7 +2,12 @@
 global $base_url;
 $icon = $base_url . file_icon_url(file_load($element['fid']));
 ?>
+<?php if (preg_match('/^http/', $element['description'])): ?>
 <img class="file-icon" alt="" title="<?php print $element['filemime']; ?>" src="<?php print $icon; ?>">
-<a href="<?php print file_create_url($element['description'] ? $element['description'] : $element['uri']); ?>">
+<a href="<?php print $element['description']; ?>">
   <?php print $element['filename']; ?>
 </a>
+<?php else: ?>
+  <img class="file-icon" alt="" title="<?php print $element['filemime']; ?>" src="<?php print $icon; ?>">
+  <?php print $element['filename']; ?>
+<?php endif; ?>
