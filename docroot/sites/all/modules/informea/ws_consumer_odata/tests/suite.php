5d14b179103a37712c087acc7394d4c5f45fa558
Cristian Romanescu <cristian@romanescu.ro>
2016-03-03 12:46:13 +0200

    Added support scripts to run the tests

---
+++ docroot/sites/all/modules/informea/ws_consumer_odata/tests/suite.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'Contacts.php';
+require_once 'CountryReports.php';
+require_once 'Decisions.php';
+require_once 'Meetings.php';
+require_once 'NationalPlans.php';
+require_once 'Sites.php';
