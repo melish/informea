f25b78ce74d0bb535b001af1e14ebfc1b442cde7
Cristian Romanescu <cristian@romanescu.ro>
2016-03-11 16:58:00 +0200

    Fixed static access to ODataConsumerConfig configuration in derived classes

--- docroot/sites/all/modules/informea/ws_consumer_odata/tests/Base.php
+++ docroot/sites/all/modules/informea/ws_consumer_odata/tests/Base.php
@@ -5,14 +5,19 @@ class ODataConsumerTestConfig extends ODataConsumerConfig {
 
   public function __construct(array $arguments) {
     parent::__construct($arguments);
+  }
+
+  static function getConfiguration() {
+    $config = parent::getConfiguration();
     // Override URL endpoints
-    self::$configuration['endpoints'][self::$ODATA_NAME_CBD]['default'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v3';
-    self::$configuration['endpoints'][self::$ODATA_NAME_ROTTERDAM]['Contacts'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v1';
-    self::$configuration['endpoints'][self::$ODATA_NAME_WHC]['default'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v1';
-    self::$configuration['endpoints'][self::$ODATA_NAME_STOCKHOLM]['Meetings'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v1';
-    self::$configuration['endpoints'][self::$ODATA_NAME_STOCKHOLM]['NationalPlans'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v1';
-    self::$configuration['endpoints'][self::$ODATA_NAME_STOCKHOLM]['Decisions'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v1';
-    self::$configuration['endpoints'][self::$ODATA_NAME_AEWA]['default'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v1';
+    $config['endpoints'][self::$ODATA_NAME_CBD]['default'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v3';
+    $config['endpoints'][self::$ODATA_NAME_ROTTERDAM]['Contacts'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v1';
+    $config['endpoints'][self::$ODATA_NAME_WHC]['default'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v1';
+    $config['endpoints'][self::$ODATA_NAME_STOCKHOLM]['Meetings'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v1';
+    $config['endpoints'][self::$ODATA_NAME_STOCKHOLM]['NationalPlans'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v1';
+    $config['endpoints'][self::$ODATA_NAME_STOCKHOLM]['Decisions'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v1';
+    $config['endpoints'][self::$ODATA_NAME_AEWA]['default'] = 'http://informea.local.ro/sites/all/modules/informea/ws_consumer_odata/tests/resources/v1';
+    return $config;
   }
 
   public function getDecisionMeetingMappingTable() {
