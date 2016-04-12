c43f2d8c860019d3c9533bd35e43555dd07b9a90
beluoctavian <beluoctavian@gmail.com>
2016-04-12 15:40:31 +0300

    Created LeoGoalMigration

--- docroot/sites/all/modules/informea/leo_migrate/includes/migrations/LeoDefaultNodeMigration.php
+++ docroot/sites/all/modules/informea/leo_migrate/includes/migrations/LeoDefaultNodeMigration.php
@@ -4,11 +4,25 @@
  * Common mappings for the Drupal 7 node migrations.
  */
 class LeoDefaultNodeMigration extends DrupalNode7Migration {
+
   public function __construct(array $arguments) {
     parent::__construct($arguments);
 
     $this->addFieldMapping('title', 'title', FALSE);
+
+    foreach ($this->taxonomyFields() as $field => $vocabulary) {
+      $this->addFieldMapping("{$field}:source_type")
+        ->defaultValue('tid');
+    }
   }
+
+  /**
+   * @return array
+   *  An associative array. Keys are fields names and values are vocabulary names.
+   */
+  protected function taxonomyFields() {
+    return array();
+  };
 
   public function map_source_term($source_tid, $taxonomy) {
     $mappings = array();
@@ -32,5 +46,16 @@ class LeoDefaultNodeMigration extends DrupalNode7Migration {
     }
 
     return NULL;
+  }
+
+  public function prepareRow($row) {
+    parent::prepareRow($row);
+    foreach ($this->taxonomyFields() as $field => $vocabulary) {
+      if (!empty($row->{$field})) {
+        foreach ($row->{$field} as $key => $value) {
+          $row->{$field}[$key] = $this->map_source_term($value, $vocabulary);
+        }
+      }
+    }
   }
 }
--- docroot/sites/all/modules/informea/leo_migrate/includes/migrations/goal/LeoGoalMigration.php
+++ docroot/sites/all/modules/informea/leo_migrate/includes/migrations/goal/LeoGoalMigration.php
@@ -1,1 +1,33 @@
 <?php
+
+class LeoGoalMigration extends LeoDefaultNodeMigration {
+
+  public function __construct(array $arguments) {
+    parent::__construct($arguments);
+
+    $this->addSimpleMappings(array_keys($this->taxonomy_fields));
+
+    $this->addSimpleMappings(array(
+      'field_official_order', 'field_official_order:language',
+    ));
+
+    $this->addUnmigratedSources(array(
+      'uid', 'revision', 'log', 'revision_uid',
+    ));
+
+    $this->addUnmigratedDestinations(array(
+    ));
+  }
+
+  protected function taxonomyFields() {
+    return [
+      'field_informea_tags' => 'thesaurus_informea',
+      'field_goal_type' => 'type_of_goal',
+      'field_goal_source' => 'goal_sources',
+    ];
+  }
+
+  function prepareRow($row) {
+    parent::prepareRow($row);
+  }
+}
