7f4e44474c3a0a98c84f704e067854cda148cd06
dragos-dumi <dumitrescu.dragos1@gmail.com>
2016-05-24 18:08:29 +0300

    Install sh from prod

--- install.sh
+++ install.sh
@@ -1,35 +1,11 @@
 #!/bin/sh
 
-# Setup a clean site in docroot/
 cd docroot/
-drush site-install -y
-drush en -y leo_theme
 
-# Save configuration to database for later usage
-drush php-script ../scripts/drupal_pre_install.php
+drush sql-drop -y
 
-drush init
-drush build
+drush sql-sync @informea.prod @self -y
 
-drush migrate-auto-register
-
-drush php-script ../scripts/drupal_post_install.php
-
-if [ "$1" == "--migrate" ]; then
-
-	echo "Running migrations ..."
-	drush mi Treaties
-	drush mi Meetings
-	drush mi Thesaurus
-
-	drush mi ActionPlans
-	drush mi NationalFocalPoints
-	drush mi NationalReports
-	drush mi Sites
-	drush mi Decisions
-
-fi
-
-drush informea-country-import
+drush devify -y
 
 drush cc all
