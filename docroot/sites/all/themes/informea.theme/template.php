fd37cb0c97b0f4d9df66fc44fc577fccc0b0f50e
beluoctavian <beluoctavian@gmail.com>
2016-06-10 16:46:01 +0300

    Prepend type of goal to targets/indicators page title

--- docroot/sites/all/themes/informea.theme/template.php
+++ docroot/sites/all/themes/informea.theme/template.php
@@ -139,7 +139,11 @@ function informea_theme_preprocess_page(&$variables) {
         if (!empty($node->field_goal_source)
           && !empty($node->field_goal_source[LANGUAGE_NONE])
           && $node->field_goal_source[LANGUAGE_NONE][0]['tid'] == $sdgs_tid) {
-          drupal_set_title(t('Target') . ' ' . drupal_get_title());
+          $node_w = entity_metadata_wrapper('node', $node);
+          if (($term = $node_w->field_goal_type->value()) && in_array($term->name, ['Target', 'Indicator'])) {
+            $tw = entity_metadata_wrapper('taxonomy_term', $term);
+            drupal_set_title($tw->label() . ' ' . $node_w->label());
+          }
           drupal_add_js(drupal_get_path('module', 'goal') . '/js/goal.js');
           drupal_add_library('informea', 'scrollto');
           drupal_add_library('informea', 'smallipop');
