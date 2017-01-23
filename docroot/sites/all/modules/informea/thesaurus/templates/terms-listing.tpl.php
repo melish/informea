0ffe185d1ac9a6c47ed0f70158f5f65624a23077
Octavian Belu <beluoctavian@gmail.com>
2017-01-23 18:25:52 +0200

    Terms listing styling

---
+++ docroot/sites/all/modules/informea/thesaurus/templates/terms-listing.tpl.php
@@ -0,0 +1,41 @@
+<div id="terms-listing-tabs">
+
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#AtoZ" aria-controls="AtoZ" role="tab" data-toggle="tab">A-Z</a></li>
+    <li role="presentation"><a href="#hierarchical" aria-controls="hierarchical" role="tab" data-toggle="tab">Hierarhical</a></li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="AtoZ">
+      <input id="terms-atoz-filter" placeholder="<?php print t('Type to filter'); ?>">
+      <div class="tab-pane-content">
+        <ul class="list-tree" id="terms-atoz">
+          <?php
+          foreach($terms_atoz as $term):
+            ?>
+            <li>
+              <a href="<?php print thesaurus_url_term($term) ;?>"><?php print $term->name; ?></a>
+            </li>
+            <?php
+          endforeach;
+          ?>
+        </ul><!-- .list-tree #terms-substantive -->
+      </div>
+    </div>
+    <div role="tabpanel" class="tab-pane" id="hierarchical">
+      <div class="tab-pane-content full-height">
+        <button type="button" class="btn btn-default pull-right" data-toggle="tree" data-target="#terms-substantive">Show all</button>
+        <ul class="list-tree" id="terms-substantive">
+          <?php
+          foreach($terms_hierarchical as $term):
+            print theme('term-tree', array('term' => $term));
+          endforeach;
+          ?>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+</div>
