7746f742dcc4c37f0cd55cfb262c448d34ae4929
Octavian Belu <beluoctavian@gmail.com>
2017-02-24 12:10:27 +0200

    Refactor glossary getSection method

--- docroot/sites/all/modules/informea/thesaurus/includes/Glossary.php
+++ docroot/sites/all/modules/informea/thesaurus/includes/Glossary.php
@@ -2,20 +2,21 @@
 
 class Glossary {
 
+  public static $sections = [
+    'default', //treaties
+    'bilateral-treaties',
+    'court-decisions',
+    'legislations',
+    'publications',
+  ];
+
   public static function getSection($path = null) {
     if (empty($path)) {
       $path = current_path();
     }
     if (preg_match('/.*(taxonomy\/term\/\d*|terms\/.*)\/*(.*)/', $path, $matches)) {
-      $allowed_sections = [
-        'default', //treaties
-        'bilateral-treaties',
-        'court-decisions',
-        'legislations',
-        'publications',
-      ];
       $section = end($matches) ?: 'default';
-      if (in_array($section, $allowed_sections)) {
+      if (in_array($section, self::$sections)) {
         return $section;
       }
     }
