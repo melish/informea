f5f5aaf17dc8cd90b26f02cf7ac975fcfe79aea4
Octavian Belu <beluoctavian@gmail.com>
2017-02-24 16:04:27 +0200

    Update path aliases for thesaurus terms

--- docroot/sites/all/modules/informea/thesaurus/includes/Thesaurus.php
+++ docroot/sites/all/modules/informea/thesaurus/includes/Thesaurus.php
@@ -2,11 +2,17 @@
 
 class Thesaurus {
 
+  /**
+   * @var int
+   *  ID of 'Thesaurus - InforMEA' vocabulary.
+   */
+  public static $vid = 14;
+
   public static $sections = [
     'default', //treaties
     'bilateral-treaties',
     'court-decisions',
-    'legislations',
+    'legislations', // @todo: replace with legislation (without 's')
     'publications',
   ];
 
@@ -21,6 +27,67 @@ class Thesaurus {
       }
     }
     return FALSE;
+  }
+
+  public static function generatePathAlias($term, $section = null) {
+    $name = slugify($term->name);
+    $alias = "terms/{$name}";
+    if (!empty($section) && $section != 'default') {
+      $alias .= "/{$section}";
+    }
+    return $alias;
+  }
+
+  /**
+   * @param $term
+   *  Taxonomy term object
+   */
+  public static function updatePathAliases() {
+    $languages = language_list();
+    $terms = taxonomy_get_tree(self::$vid);
+    foreach ($terms as $term) {
+      foreach (self::$sections as $section) {
+        $alias = self::generatePathAlias($term, $section);
+        foreach ($languages as $langKey => $language) {
+          $source = "taxonomy/term/{$term->tid}";
+          if ($section != 'default') {
+            $source .= "/{$section}";
+          }
+          $path = path_load([
+            'source' => $source,
+            'language' => $langKey,
+          ]);
+          if (empty($path)) {
+            // Path alias does not exist. Create a new one
+            $path = [
+              'source' => $source,
+              'alias' => $alias,
+              'language' => $langKey,
+            ];
+            path_save($path);
+          }
+          elseif ($path['alias'] != $alias) {
+            // The path alias exists, but it is incorrect.
+            // Update the alias and redirect the old one.
+            $redirect = new stdClass();
+            redirect_object_prepare($redirect, [
+              'source' => $path['alias'],
+              'redirect' => $alias,
+              'language' => $langKey,
+            ]);
+
+            if (function_exists('drush_log')) {
+              drush_log("Redirect {$langKey}/{$path['alias']} to {$langKey}/{$alias}", "ok");
+            }
+
+            $path['alias'] = $alias;
+            path_save($path);
+
+            redirect_save($redirect);
+          }
+        }
+      }
+    }
   }
 
 }
